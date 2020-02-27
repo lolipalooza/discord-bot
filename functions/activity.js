51eb97dee648b9f953bd58bfdb450d3a5dc3b1dd
lolipalooza <mumifag_lalolanda@hotmail.com>
2020-02-27 12:48:47 -0400

    Update

--- functions/activity.js
+++ functions/activity.js
@@ -1,4 +1,5 @@
 const fs = require('fs')
+const utils = require('../functions/utils')
 
 var prev_activity = ""
 
@@ -21,6 +22,7 @@ module.exports.set = (client, activity) => {
 module.exports.get = () => {
 	let activities = JSON.parse( fs.readFileSync('./data.json') ).bot_activities
 	let id, type, name, cond
+	let rand = utils.rand
 	
 	type = ["PLAYING", "WATCHING", "LISTENING", "STREAMING"][rand(0,2)] // Streaming excluded
 	do {
@@ -34,45 +36,4 @@ module.exports.get = () => {
 		type: type,
 		name: name,
 	}
-}
-
-function rand (min,max) {
-    return Math.floor((Math.random() * (max-min+1)) + min)
-}
-
-// Debugging only
-function getMinsMaxs (min, max) {
-	var _min = (max-min)/2,
-		_max = (max-min)/2;
-	for(var i=0; i<500; i++){
-		var _rand = rand (min, max)
-		_min = (_rand<_min)?_rand:_min
-		_max = (_rand>_max)?_rand:_max
-	}
-	console.log({min:_min,max:_max})
-}
-
-function parseExpressions (string) {
-	/*
-	// parse the expression "rand(min,max)"
-	let numbers = string.replace(/.*?rand\s*?\(\s*?(\d+)\s*?,\s*?(\d+)\s*?\).*?/i, "$1,$2")
-	let _rand = rand( parseInt(numbers.split(",")[0]), parseInt(numbers.split(",")[1]) )
-	string = string.replace(/rand\s*?\(\s*?(\d+)\s*?,\s*?(\d+)\s*?\)/i, _rand)
-	*/
-	
-	// If the string match any of the regexps here, then use eval(<match>) on those matches
-	let expressions = [
-		// Regexp											Expression to parse
-		String.raw`rand\s*?\(\s*?\d+\s*?,\s*?\d+\s*?\)`,	// rand(min,max)
-		String.raw`\[[\w\s,']+\]\[.+?\]`,					// rand(min,max)
-	]
-	
-	for (let i=0; i<expressions.length; i++)
-	{
-		let expr = string.match( new RegExp(expressions[i]) )[0]		// get only the isolated desired expression to parse
-		expr = eval(expr)												// parsing the expression
-		string = string.replace( new RegExp(expressions[i]), expr )		// replacing the expression with the parsed one
-	}
-	
-	return string
 }
