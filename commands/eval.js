97fce0dc420489f5313cc1ead8941b953a541f34
lolipalooza <mumifag_lalolanda@hotmail.com>
2020-02-27 16:31:35 -0400

    Update

--- commands/eval.js
+++ commands/eval.js
@@ -1,7 +1,8 @@
 exports.run = async (client, message, args) => {
 	
-	/*try {
-		let expr = "ret=" + message.content.match( /^.eval (.+?)$/ )[1]
+	try {
+		let expr = args.join(" ")
+		expr = "ret=" + expr.match( /^(.+?)$/ )[1]
 		let eval_expr = eval(expr)
 		let returned_value
 		
@@ -11,9 +12,9 @@ exports.run = async (client, message, args) => {
 			
 		} else if (typeof(eval_expr) == "object") {
 			
-			returned_value = "{ "
+			returned_value = "{\n"
 			for (const [key, value] of Object.entries(eval_expr))
-				returned_value += key + ": " + value + "\n  "
+				returned_value += key + ": " + value + "\n"
 			returned_value += "}"
 			
 		}
@@ -21,15 +22,13 @@ exports.run = async (client, message, args) => {
 		message.channel.send("```" + returned_value + "```")
 	} catch (e) {
 		message.channel.send("`error`\n" + "```" + e + "```")
-	}*/
-	
-	console.log({message:message, args:args})
+	}
 }
 
 exports.help = {
 	name: "eval",
 	category: "Randurr",
 	description: "Testea código javascript (¡funcionalidad en fase beta!).",
-	usage: "eval( <javascript expression> )",
+	usage: "eval <javascript expression>",
 	aliases: []
 }
--- events/message.js
+++ events/message.js
@@ -5,15 +5,10 @@ module.exports = (client, message) => {
 	let words = "(Shelitos|Hielitos|Yelitos|Cirno|" + client.user.id + "|Hielos|hielocos|yelocos|yelos|shelos|chirunito|chiruno|nalgas heladas)"
 	let shelitos_pattern = new RegExp("(^|[^!])" + words, 'gi')
 	
-	if ( shelitos_pattern.test(message.content) && message.author.id != client.user.id ) // Are they talking to me?
-	{
+	if ( /^yelitos di:\s*?(.+?)$/i.test(message.content) ) {
+		message.channel.send( message.content.match( /^yelitos di:\s*?(.+?)$/i )[1] )
+	} else if ( shelitos_pattern.test(message.content) && message.author.id != client.user.id ) { // Are they talking to me?
 		message.channel.send('`' + message.author.username + '` ' + ['Cy.','Ño.'][rand(0,1)])
-	}
-	else
-	{
-		if ( /^!yelitos di:\s*?(.+?)$/i.test(message.content) ) {
-			message.channel.send( message.content.match( /^!yelitos di:\s*?(.+?)$/i )[1] )
-		}
 	}
 	
 	let prefix = "!"
