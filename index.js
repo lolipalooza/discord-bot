bd8c48f8d19247135c78a39ae2e3e80092e4f8a0
lolipalooza <mumifag_lalolanda@hotmail.com>
2020-02-27 11:47:53 -0400

    Update

--- index.js
+++ index.js
@@ -51,47 +51,6 @@ client.on('message', msg => {
 	}
 })
 
-function rand (min,max) {
-    return Math.floor((Math.random() * (max-min+1)) + min)
-}
-
-// Debugging only
-function getMinsMaxs (min, max) {
-	var _min = (max-min)/2,
-		_max = (max-min)/2;
-	for(var i=0; i<500; i++){
-		var _rand = rand (min, max)
-		_min = (_rand<_min)?_rand:_min
-		_max = (_rand>_max)?_rand:_max
-	}
-	console.log({min:_min,max:_max})
-}
-
-function parseExpressions (string) {
-	/*
-	// parse the expression "rand(min,max)"
-	let numbers = string.replace(/.*?rand\s*?\(\s*?(\d+)\s*?,\s*?(\d+)\s*?\).*?/i, "$1,$2")
-	let _rand = rand( parseInt(numbers.split(",")[0]), parseInt(numbers.split(",")[1]) )
-	string = string.replace(/rand\s*?\(\s*?(\d+)\s*?,\s*?(\d+)\s*?\)/i, _rand)
-	*/
-	
-	// If the string match any of the regexps here, then use eval(<match>) on those matches
-	let expressions = [
-		// Regexp											Expression to parse
-		String.raw`rand\s*?\(\s*?\d+\s*?,\s*?\d+\s*?\)`,	// rand(min,max)
-		String.raw`\[[\w\s,']+\]\[.+?\]`,					// rand(min,max)
-	]
-	
-	for (let i=0; i<expressions.length; i++)
-	{
-		let expr = string.match( new RegExp(expressions[i]) )[0]		// get only the isolated desired expression to parse
-		expr = eval(expr)												// parsing the expression
-		string = string.replace( new RegExp(expressions[i]), expr )		// replacing the expression with the parsed one
-	}
-	
-	return string
-}
-
 function richEmbedFortuna(message) {
 	let fortunas = JSON.parse( fs.readFileSync('./data.json') ).fortunas
 	
